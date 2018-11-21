7edee6a4da688b50aa77aa7fac6001b31bd85f55
Joann Pan <joannlpan@gmail.com>
2018-11-21 19:14:53 +0000

    worked on CLI

--- lib/bookfinder/CLI.rb
+++ lib/bookfinder/CLI.rb
@@ -1,7 +1,22 @@
 class Bookfinder::CLI
   
 def call
+  list_books
+  menu
+
+end
+
+def list_books
   puts "Welcome to the Bookfinder CLI app!"
+  puts "list of books"
+end
+
+def menu
+  puts "menu options"
+  puts "“Enter the number of the book you’d like to learn more about. Enter exit to leave the program."
+  input = gets.strip
+  case input
+   
 end
 
 end
