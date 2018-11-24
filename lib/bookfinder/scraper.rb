970c57a0b94181a476fa6b630955e5e19a7b0d8c
Joann Pan <joannlpan@gmail.com>
2018-11-24 20:21:43 +0000

    scraping book landing

--- lib/bookfinder/scraper.rb
+++ lib/bookfinder/scraper.rb
@@ -12,10 +12,19 @@ def self.scrape_books
     title = book.css("div.title").text.strip
     author = book.css("div.contributor").text.strip
     image = book.css("div a img").attribute("src").value
+    path = book.css("div.img a").attribute("href").value
+    new_book_landing = Nokogiri::HTML(open("https://www.penguinrandomhouse.com#{path}"
+  ))
+    
     
     binding.pry
     end
 end
+
+#def self.scrape_individual_book
+#  page = Nokogiri::HTML(open("https://www.penguinrandomhouse.com#{path}"))
+ # binding_pry
+#end
 
 end
 
