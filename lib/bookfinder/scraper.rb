74cfe4c07fc26e573223f4bb77b040d307243f2e
Joann Pan <joannlpan@gmail.com>
2018-11-24 19:38:52 +0000

    worked on scraper

--- lib/bookfinder/scraper.rb
+++ lib/bookfinder/scraper.rb
@@ -1,42 +1,21 @@
+require 'pry'
 class Bookfinder::Scraper 
-
-
-
-
-def make_books
-  
-end 
-#create new books and assign them attributes
-
 
 def self.scrape_books
    doc = Nokogiri::HTML(open("https://www.penguinrandomhouse.com/books/award-winners-fiction"))
    books = doc.css("div.book")
-   books.each do |book|
+   
+   collection_of_books = {}
+  
+   
+   books.each do |book| 
     title = book.css("div.title").text.strip
     author = book.css("div.contributor").text.strip
+    image = book.css("div a img").attribute("src").value
     
     binding.pry
-  end
-end
-  
-
+    end
 end
 
-#maybe create hashes of books?
-#then assign attributes to symbols
-   
-   
-   
-   
-   
- 
-  
-
-
-#book_1.name = title of book
-#book_1.author = author name
-#book_1.genre = genre
-#book_1.description = description
-#book_1.url = url
+end
 
