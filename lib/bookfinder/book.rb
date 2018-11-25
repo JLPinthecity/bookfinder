a3f82a11ec28a4972149209400221ff2dbbd4e89
Joann Pan <joannlpan@gmail.com>
2018-11-25 04:18:55 +0000

    book.rb

--- lib/bookfinder/book.rb
+++ lib/bookfinder/book.rb
@@ -3,17 +3,13 @@ require 'pry'
 class Bookfinder::Book
   attr_accessor :title, :author, :genre, :description, :url
   
-@@books = ["1", "2", "3"]
-
-
-
-def add_book
-  @@books << self
+def initialize(attributes)
+  attributes.each do |key, value|
+    self.send(("#{key}="), value)
+  end
 end
 
-def self.all 
-  @@books
-end
+
 
 end
 
--- lib/bookfinder/scraper.rb
+++ lib/bookfinder/scraper.rb
@@ -2,34 +2,25 @@ require 'pry'
 class Bookfinder::Scraper 
 
 def self.scrape_books
-   doc = Nokogiri::HTML(open("https://www.penguinrandomhouse.com/books/award-winners-fiction"))
-   books = doc.css("div.book")
-   
-   collection_of_books = {}
+  doc = Nokogiri::HTML(open("https://www.penguinrandomhouse.com/books/award-winners-fiction"))
+  new_book_landing = Nokogiri::HTML(open("#{url}"))
   
-    books.each do |book| 
+  books = {}
+    
+  doc.css("div.book").each do |book| 
     title = book.css("div.title").text.strip
-    author = book.css("div.contributor").text.strip
-    image = book.css("div a img").attribute("src").value
-    path = book.css("div.img a").attribute("href").value
-    url = print "https://www.penguinrandomhouse.com#{path}"
     
-    
-    new_book_landing = Nokogiri::HTML(open("https://www.penguinrandomhouse.com#{path}"
-  ))
-    category_of_fiction = new_book_landing.css("div#product-top-category a").text.split[0] + " fiction"
-    description = new_book_landing.css("div#mobile-about-the-book p").text.split("\n\t")[0]
-    published_date = 
-    binding.pry
+    books[title.to_sym] = {
+      
+    author: book.css("div.contributor").text.strip,
+    image: book.css("div a img").attribute("src").value,
+    path: book.css("div.img a").attribute("href").value,
+    url:  "https://www.penguinrandomhouse.com#{path}",
+    category_of_fiction: new_book_landing.css("div#product-top-category a").text.split[0] + " fiction",
+    description: new_book_landing.css("div#mobile-about-the-book p").text.split("\n\t")[0]
+    }
+      binding.pry
     end
 end
 
-#def self.scrape_individual_book
-#  page = Nokogiri::HTML(open("https://www.penguinrandomhouse.com#{path}"))
- # binding_pry
-#end
-
 end
-
-
-#pry(Bookfinder::Scraper)> new_book_landing.css("div.product-format-tblt p span").text
